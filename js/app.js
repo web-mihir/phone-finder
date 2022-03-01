dc35298d6d3850448523da69ccb2efe2d1f761a7
samiranGithub <samirankumarroy24@gmail.com>
2022-03-01 20:39:35 +0600

    added searchPhone function

--- js/app.js
+++ js/app.js
@@ -1,37 +1,55 @@
 const searchBtn = document.getElementById('search-btn');
 
+
 const apiCall = (searchText) => {
    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
    fetch(url).then(res => res.json()).then(data => {
-      //return data.data;
+
       return displaySearchResults(data.data);
+
    })
 }
 
+
+const searchPhone = () => {
+   const searchField = document.getElementById('search-field');
+   const searchInputValue = searchField.value;
+
+   if (searchInputValue == "") {
+      document.querySelector('.search-err').innerHTML = "Write Somthing...";
+   } else {
+      apiCall(searchInputValue);
+   }
+}
+
+
 const displaySearchResults = (items) => {
    const displayResults = document.querySelector('.search-results');
-   const dataValue = items.map((phone) => {
-      return `
-      <div class="col-lg-4 col-sm-12">
-         <div class="card">
-         <img src="${phone.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-               <h5 class="card-title">${phone.phone_name}</h5>
-               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-               <button id="item-details" class="btn btn-primary">Details</button>
-            </div>
-         </div>
-      </div>
-      `;
-   });
 
-   displayResults.innerHTML = dataValue.join("");
+   if (items.length < 0) {
+      document.querySelector('.search-err').innerHTML = "not Somthing...";
+   } else {
+      const dataValue = items.map((item) => {
+
+         return `
+            <div class="col-lg-4 col-sm-12">
+               <div class="card">
+               <img src="${item.image}" class="card-img-top" alt="...">
+                  <div class="card-body">
+                     <h5 class="card-title">${item.phone_name}</h5>
+                     <button id="item-details" class="btn btn-primary">Details</button>
+                  </div>
+               </div>
+            </div>
+            `;
+      });
+
+      displayResults.innerHTML = dataValue.join("");
+   }
 }
 
 searchBtn.addEventListener('click', () => {
-   const searchField = document.getElementById('search-field');
-   const searchInputValue = searchField.value;
-   apiCall(searchInputValue);
+   searchPhone();
 })
 
 
