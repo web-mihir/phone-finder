a36e841c9c936f2359c8135476a1bacb340824bc
samiranGithub <samirankumarroy24@gmail.com>
2022-03-01 21:05:39 +0600

    small changes in displaySearchResults function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
 const searchBtn = document.getElementById('search-btn');
 
-
 const apiCall = (searchText) => {
    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
    fetch(url).then(res => res.json()).then(data => {
-
       return displaySearchResults(data.data);
-
    })
 }
 
@@ -16,7 +13,7 @@ const searchPhone = () => {
    const searchInputValue = searchField.value;
 
    if (searchInputValue == "") {
-      document.querySelector('.search-err').innerHTML = "Write Somthing...";
+      document.querySelector('.search-err').innerText = "Write Somthing...";
    } else {
       apiCall(searchInputValue);
    }
@@ -24,27 +21,28 @@ const searchPhone = () => {
 
 
 const displaySearchResults = (items) => {
-   const displayResults = document.querySelector('.search-results');
-
-   if (items.length < 0) {
-      document.querySelector('.search-err').innerHTML = "not Somthing...";
-   } else {
-      const dataValue = items.map((item) => {
-
-         return `
+   let displayResults = document.querySelector('.search-results');
+   if (items.length > 0) {
+      displayResults.innerHTML = "";
+      items.forEach(item => {
+         console.log(item);
+         displayResults.innerHTML += `
             <div class="col-lg-4 col-sm-12">
                <div class="card">
                <img src="${item.image}" class="card-img-top" alt="...">
                   <div class="card-body">
                      <h5 class="card-title">${item.phone_name}</h5>
-                     <button id="item-details" class="btn btn-primary">Details</button>
+                     <button id="item-details" onclick="displayDetails(${item.slug})" class="btn btn-primary">Details</button>
                   </div>
-               </div>
+               </div>   
             </div>
-            `;
+         `;
       });
 
-      displayResults.innerHTML = dataValue.join("");
+      document.querySelector('.search-err').innerText = "";
+   } else {
+      displayResults.innerHTML = "";
+      document.querySelector('.search-err').innerText = "No results found.";
    }
 }
 
@@ -54,3 +52,21 @@ searchBtn.addEventListener('click', () => {
 
 
 
+
+
+      // const dataValue = items.map((item) => {
+
+      //    return `
+      //       <div class="col-lg-4 col-sm-12">
+      //          <div class="card">
+      //          <img src="${item.image}" class="card-img-top" alt="...">
+      //             <div class="card-body">
+      //                <h5 class="card-title">${item.phone_name}</h5>
+      //                <button id="item-details" class="btn btn-primary">Details</button>
+      //             </div>
+      //          </div>
+      //       </div>
+      //       `;
+      // });
+
+      // displayResults.innerHTML = dataValue.join("");
